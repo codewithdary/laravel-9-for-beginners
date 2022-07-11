7e84a7c129297134f83a49b56bd237a69ac17854
codewithdary <info@darynazar.com>
2022-07-11 14:44:16 +0200

    [FEATURE][23-retrieve-data-eloquent]

--- resources/views/blog/index.blade.php
+++ resources/views/blog/index.blade.php
@@ -1,3 +1,16 @@
-<h1>
-    Index Method!
-</h1>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+</head>
+<body>
+    @forelse ($posts as $post)
+        {{ $loop->parent }}
+    @empty
+        <p>No posts have been set</p>
+    @endforelse
+</body>
+</html>
